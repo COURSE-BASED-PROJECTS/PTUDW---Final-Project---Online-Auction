bfc3fac584582eb8573c72f7164dd4e3ca8889bf
Nguyen Duc Huy <duchuy040421@gmail.com>
2021-12-05 08:48:16 +0700

    update home view, main layout

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 import express from 'express'
-const app = express()
-const port = 3000
+import morgan from 'morgan'
+import { engine } from 'express-handlebars';
+import hbs_sections from 'express-handlebars-sections';
 
-app.get('/',function (req, res) {
-    res.send('Hello World!')
+const app = express();
+const port = 3000;
+
+// use public file
+app.use(express.static('public'));
+
+// use morgan
+app.use(morgan('dev'))
+
+//set up handlebars
+app.engine('.hbs', engine({
+    extname:'.hbs',
+    helpers: {
+        section: hbs_sections(),
+    }
+}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+// use urlencoded
+app.use(express.urlencoded({
+    extended:true
+}));
+
+
+// controller
+app.get('/', function (req, res)  {
+    res.render('home',{
+        layout:'main'
+    });
 })
 
+// add listen port
 app.listen(port, function() {
     console.log(`Example app listening at http://localhost:${port}`)
 })

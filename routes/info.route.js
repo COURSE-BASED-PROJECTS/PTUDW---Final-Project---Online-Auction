c5966571aebd320f1ad61a68cf9fead7a4ca82a6
nhthong2001 <nhthong30@gmail.com>
2021-12-24 01:59:23 +0700

    thêm cột dob cho bảng account db, sửa thêm ô nhập ngày sinh cho màn hình đăng kí, thêm màn hình xem thông tin cá nhân và đổi mật khẩu

--- routes/info.route.js
+++ routes/info.route.js
@@ -3,20 +3,73 @@ import productFavoriteModel from "../models/productFavorite.model.js";
 import productHistoryModel from "../models/productHistory.model.js";
 import productAuctionModel from "../models/productOnAuction.model.js";
 import productWonModel from "../models/productWon.model.js";
+import accountModel from "../models/account.model.js";
+import moment from "moment";
+import bcrypt from "bcrypt";
 
 
 const router = express.Router();
 
+router.get('/reviewProfile', async function (req, res) {
+    const username = req.session.authAccount.username;
+    const user = await accountModel.findByUsername(username);
+    res.render('vwInfo/profileAccount', {
+        layout: 'Signin_login',
+        user,
+        isProfile: true,
+    });
+});
 
-router.get('/reviewHistory',async function (req,res){
+router.post('/reviewProfile', async function (req, res) {
+    //console.log(req.headers.referer);
+    req.body.username = req.session.authAccount.username;
+    req.body.dob = moment(req.body.dob,'DD/MM/YYYY').format('YYYY-MM-DD');
+
+    await accountModel.updateInfoAccount(req.body);
+    res.redirect('/info/reviewProfile');
+});
+
+router.get('/reviewProfile/changePassword', function (req, res) {
+    res.render('vwInfo/changePassword', {
+        layout: 'Signin_login',
+    });
+});
+router.post('/reviewProfile/changePassword', async function (req, res) {
+    const rawPassword = req.body.newPass;
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(rawPassword, salt);
+
+    const username = req.session.authAccount.username;
+    const entity = {
+        username: username,
+        password: hash
+    }
+    await accountModel.updatePassword(entity);
+    res.redirect('/info/reviewProfile');
+});
+
+// router.get('/reviewProfile/is-changed',async function (req,res){
+//     const username = req.session.authAccount.username;
+//     const name = req.query.name;
+//
+//     const account = await accountModel.findByUsername(username);
+//
+//     if(account === null){
+//         return res.json(true);
+//     }
+//
+//     res.json(false);
+// });
+
+router.get('/reviewHistory', async function (req, res) {
     const username = req.session.authAccount.username;
     const list = await productHistoryModel.findWonProduct(username);
     const point = await productHistoryModel.getPointAccount(username);
     console.log(point);
 
-    res.render('vwInfo/reviewHistory',{
-        layout:'main',
-        isHistory:true,
+    res.render('vwInfo/reviewHistory', {
+        layout: 'main',
+        isHistory: true,
         list,
         isEmpty: list.length === 0,
         point,
@@ -24,40 +77,40 @@ router.get('/reviewHistory',async function (req,res){
     });
 });
 
-router.get('/updateInfo',async function (req,res){
-    res.render('vwInfo/updateInfo',{
-        layout:'Signin_login',
-        isUpdateInfo:true,
+router.get('/updateInfo', async function (req, res) {
+    res.render('vwInfo/updateInfo', {
+        layout: 'Signin_login',
+        isUpdateInfo: true,
     });
 });
 
-router.get('/favouriteProduct',async function (req,res){
+router.get('/favouriteProduct', async function (req, res) {
     const list = await productFavoriteModel.findFavorite(req.session.authAccount.username)
 
-    res.render('vwInfo/favouriteProduct',{
-        layout:'main',
-        isFavorite:true,
+    res.render('vwInfo/favouriteProduct', {
+        layout: 'main',
+        isFavorite: true,
         list,
         isEmpty: list.length === 0,
     });
 });
 
-router.post('/cancelFavorite/:ProID',async function (req,res){
+router.post('/cancelFavorite/:ProID', async function (req, res) {
     const username = req.session.authAccount.username;
     const ProID = req.params.ProID;
 
-    await productFavoriteModel.cancelFavorite(ProID,username);
+    await productFavoriteModel.cancelFavorite(ProID, username);
 
     const url = req.headers.referer || '/';
     res.redirect(url);
 });
 
-router.post('/addFavorite/:ProID',async function (req,res){
+router.post('/addFavorite/:ProID', async function (req, res) {
     const username = req.session.authAccount.username;
     const ProID = req.params.ProID;
     const entity = {
-        username:username,
-        ProID:ProID
+        username: username,
+        ProID: ProID
     }
     await productFavoriteModel.addFavorite(entity);
 
@@ -65,52 +118,52 @@ router.post('/addFavorite/:ProID',async function (req,res){
     res.redirect(url);
 });
 
-router.get('/onlineAuction',async function (req,res){
+router.get('/onlineAuction', async function (req, res) {
     const username = req.session.authAccount.username;
     const list = await productAuctionModel.findOnAuction(username);
 
-    res.render('vwInfo/auctionProduct',{
-        layout:'main',
-        isOnlineAuction:true,
+    res.render('vwInfo/auctionProduct', {
+        layout: 'main',
+        isOnlineAuction: true,
         list,
         isEmpty: list.length === 0
     });
 });
 
-router.get('/wonProduct',async function (req,res){
+router.get('/wonProduct', async function (req, res) {
     const username = req.session.authAccount.username;
     const list = await productWonModel.findWonProduct(username);
 
-    res.render('vwInfo/wonProduct',{
-        layout:'main',
-        isWon:true,
+    res.render('vwInfo/wonProduct', {
+        layout: 'main',
+        isWon: true,
         list,
         isEmpty: list.length === 0,
     });
 });
 
-router.post('/wonProduct/:ProID',async function (req,res){
+router.post('/wonProduct/:ProID', async function (req, res) {
     const username = req.session.authAccount.username;
     const ProID = req.params.ProID;
-    await productWonModel.updateCommentBidder(username,ProID,req.body.comment);
+    await productWonModel.updateCommentBidder(username, ProID, req.body.comment);
 
     const url = req.headers.referer || '/';
     res.redirect(url);
 });
 
-router.post('/wonProduct/like/:ProID',async function (req,res){
+router.post('/wonProduct/like/:ProID', async function (req, res) {
     const username = req.session.authAccount.username;
     const ProID = req.params.ProID;
-    await productWonModel.updateLike(username,ProID);
+    await productWonModel.updateLike(username, ProID);
 
     const url = req.headers.referer || '/';
     res.redirect(url);
 });
 
-router.post('/wonProduct/dislike/:ProID',async function (req,res){
+router.post('/wonProduct/dislike/:ProID', async function (req, res) {
     const username = req.session.authAccount.username;
     const ProID = req.params.ProID;
-    await productWonModel.updateDislike(username,ProID);
+    await productWonModel.updateDislike(username, ProID);
 
     const url = req.headers.referer || '/';
     res.redirect(url);

eb4826a218c014bf0ea434d91a33714cbf3f1bfc
Nguyen Duc Huy <duchuy040421@gmail.com>
2021-12-30 19:32:44 +0700

    remove redundant code

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ import knexObj from "knex";
 
 export const connectionInfo = {
     host : '127.0.0.1',
-    port : 3306,
+    port : 3333,
     user : 'root',
     password : '',
     database : 'onlineauction'
